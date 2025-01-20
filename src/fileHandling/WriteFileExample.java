8236651f1b9039b5c82cdc8fcb73d05407b43332
Amisha0971 <amishaninawe971@gmail.com>
2025-01-20 22:18:15 +0530

    first commit

---
+++ src/fileHandling/WriteFileExample.java
@@ -0,0 +1,20 @@
+package fileHandling;
+
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class WriteFileExample {
+
+	public static void main(String[] args) {
+		try {
+            FileWriter writer = new FileWriter("example.txt");
+            writer.write("Hello, this is a file handling example.");
+            writer.close();
+            System.out.println("Successfully wrote to the file.");
+        } catch (IOException e) {
+            System.out.println("An error occurred.");
+            e.printStackTrace();
+        }
+	}
+
+}
